b9adc36206899452cd87c00d8345586e47445b49
zhao <1208390864@qq.com>
2025-08-13 20:48:06 +0800

    perf：定时任务优化学习记录修改-增加线程池优化

--- tj-learning/src/main/java/com/tianji/learning/utils/LearningRecordDelayTaskHandler.java
+++ tj-learning/src/main/java/com/tianji/learning/utils/LearningRecordDelayTaskHandler.java
@@ -19,8 +19,7 @@ import javax.annotation.PreDestroy;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.DelayQueue;
+import java.util.concurrent.*;
 
 @Slf4j
 @Component
@@ -33,6 +32,8 @@ public class LearningRecordDelayTaskHandler {
     private final DelayQueue<DelayTask<RecordTaskData>> queue = new DelayQueue<>();
     private final static String RECORD_KEY_TEMPLATE = "learning:record:{}";
     private static volatile boolean begin = true;
+
+    static ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(16, 20, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>(10));
 
     // 从ForkJoinPool.commonPool()中获取一个线程执行handleDelayTask方法
     @PostConstruct
@@ -57,35 +58,44 @@ public class LearningRecordDelayTaskHandler {
      * queue.take()会阻塞这个线程直到有到期的任务；
      * 如果任务未到期（不到20秒），线程会一直阻塞等待；
      * while (begin)循环确保只要服务在运行，就会持续处理任务；
+     * 进阶：
+     * 1.使用线程池执行任务，避免阻塞主线程；
+     * 2.使用CompletableFuture异步处理任务，避免阻塞主线程；
      */
     public void handleDelayTask() {
         while (begin) {
             try {
                 // 1.获取到期的延迟任务
                 DelayTask<RecordTaskData> task = queue.take();
-                RecordTaskData data = task.getData();
-                // 2.查询Redis缓存
-                LearningRecord record = readRecordCache(data.getLessonId(), data.getSectionId());
-                if (record == null) {
-                    continue;
-                }
-                // 3.比较数据，moment值
-                if (!Objects.equals(data.getMoment(), record.getMoment())) {
-                    // 不一致，说明用户还在持续提交播放进度，放弃旧数据
-                    continue;
-                }
+                poolExecutor.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        RecordTaskData data = task.getData();
+                        // 2.查询Redis缓存
+                        LearningRecord record = readRecordCache(data.getLessonId(), data.getSectionId());
+                        if (record == null) {
+                            return;
+                        }
+                        // 3.比较数据，moment值
+                        if (!Objects.equals(data.getMoment(), record.getMoment())) {
+                            // 不一致，说明用户还在持续提交播放进度，放弃旧数据
+                            return;
+                        }
 
-                // 4.一致，持久化播放进度数据到数据库
-                // 4.1.更新学习记录的moment
-                record.setFinished(null);
-                recordMapper.updateById(record);
-                // 4.2.更新课表最近学习信息
-                LearningLesson lesson = new LearningLesson();
-                lesson.setId(data.getLessonId());
-                lesson.setLatestSectionId(data.getSectionId());
-                lesson.setLatestLearnTime(LocalDateTime.now());
-                lessonService.updateById(lesson);
-                log.info("更新学习记录的缓存数据");
+                        // 4.一致，持久化播放进度数据到数据库
+                        // 4.1.更新学习记录的moment
+                        record.setFinished(null);
+                        recordMapper.updateById(record);
+                        // 4.2.更新课表最近学习信息
+                        LearningLesson lesson = new LearningLesson();
+                        lesson.setId(data.getLessonId());
+                        lesson.setLatestSectionId(data.getSectionId());
+                        lesson.setLatestLearnTime(LocalDateTime.now());
+                        lessonService.updateById(lesson);
+                        log.info("更新学习记录的缓存数据");
+                    }
+                });
+
             } catch (Exception e) {
                 log.error("处理延迟任务发生异常", e);
             }
