66ab8713366c7689c9dfc591662f89a3800ae9cc
zhao <1208390864@qq.com>
2025-08-26 16:44:40 +0800

    chore：积分系统初始化

---
+++ tj-learning/src/main/java/com/tianji/learning/service/IPointsBoardSeasonService.java
@@ -0,0 +1,16 @@
+package com.tianji.learning.service;
+
+import com.tianji.learning.domain.po.PointsBoardSeason;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zhao
+ * @since 2025-08-26
+ */
+public interface IPointsBoardSeasonService extends IService<PointsBoardSeason> {
+
+}
