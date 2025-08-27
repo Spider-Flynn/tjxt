c05163aa179c8e1984e4db8dac784dfa7b535e7f
zhao <1208390864@qq.com>
2025-08-27 10:20:58 +0800

    feat（points）：签到接口完成

---
+++ tj-learning/src/main/java/com/tianji/learning/constants/RedisConstants.java
@@ -0,0 +1,10 @@
+package com.tianji.learning.constants;
+
+/**
+ * @Description: 签到相关的redis常量
+ * @Author: zhao
+ * @Since: 2025/8/27 - 10:00
+ */
+public class RedisConstants {
+    public static final String SIGN_RECORD_KEY_PREFIX = "sign:record:";
+}
